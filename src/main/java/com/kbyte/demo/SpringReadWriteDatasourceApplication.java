acfb0ca1546e3d240c0de5a99af7dfe283d1fcbe
Hao <kuangbyte@gmail.com>
2021-06-18 23:52:58 -0400

    First commit

---
+++ src/main/java/com/kbyte/demo/SpringReadWriteDatasourceApplication.java
@@ -0,0 +1,13 @@
+package com.kbyte.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReadWriteDatasourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReadWriteDatasourceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kbyte/demo/SpringReadWriteDatasourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kbyte.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReadWriteDatasourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
